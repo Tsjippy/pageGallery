33780f5686270276651e638145a96416bc7afeb9
Tsjippy <ewaldha@hotmail.com>
2024-10-11 07:18:30 +0100

    changed: redering of asset urls

--- php/enqueue.php
+++ php/enqueue.php
@@ -3,5 +3,5 @@ namespace SIM\PAGEGALLERY;
 use SIM;
 
 add_action( 'wp_enqueue_scripts', function(){
-	wp_register_script('sim_page_gallery_script', plugins_url('js/page_gallery.min.js', __DIR__), array('sim_formsubmit_script'), MODULE_VERSION, true);
+	wp_register_script('sim_page_gallery_script', SIM\pathToUrl(MODULE_PATH.'js/page_gallery.min.js'), array('sim_formsubmit_script'), MODULE_VERSION, true);
 });
