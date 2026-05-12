d73726d691e8314a8d808988e701321407a67a85
Tsjippy <ewaldha@hotmail.com>
2026-05-12 10:57:21 +0200

    changed: permission callback for rest api

--- blocks/rest_api.php
+++ blocks/rest_api.php
@@ -12,11 +12,17 @@ function blockRestApi() {
 		array(
 			'methods' 				=> 'POST',
 			'callback' 				=> __NAMESPACE__.'\pageGalleryBlock',
-			'permission_callback' 	=> '__return_true',
+			'permission_callback' 	=> '__return_true', // Allow non-logged in users to access this endpoint
 		)
 	);
 }
 
+/**
+ * Displays the page gallery based on the provided request parameters.
+ *
+ * @param \WP_REST_Request $wpRestRequest The REST request object.
+ * @return array The page gallery data.
+ */
 function pageGalleryBlock($wpRestRequest){
 	$params			= $wpRestRequest->get_params();
 
@@ -41,6 +47,12 @@ function pageGalleryBlock($wpRestRequest){
 
 // Allow non-logged in use
 add_filter('tsjippy_allowed_rest_api_urls', __NAMESPACE__.'\restApiUrls');
+/**
+ * Adds the page gallery URLs to the list of allowed REST API URLs.
+ *
+ * @param array $urls The list of allowed REST API URLs.
+ * @return array The updated list of allowed REST API URLs.
+ */
 function restApiUrls($urls){
 	$urls[]		= RESTAPIPREFIX.'/pagegallery/show_page_gallery';
 
